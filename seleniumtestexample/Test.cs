95d580efe1f2a234f8c6b342d14cd3496aa05b77
belensarabia <belensarabia@gmail.com>
2018-11-23 18:50:38 +0100

    Inserción inicial en el repositorio del módulo seleniumtestexample

---
+++ seleniumtestexample/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace seleniumtestexample
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
